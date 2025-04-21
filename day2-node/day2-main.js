87c79104b2a26215cb6cdf1c57d69b53d68ec375
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-04-21 20:32:58 +0600

    understanding URL Module

--- day2-node/day2-main.js
+++ day2-node/day2-main.js
@@ -1,4 +1,6 @@
-let http = require("http");
+/* 1. Request Response model works in node.js
+
+ let http = require("http");
 
 let server = http.createServer((request, response) => {
     if(request.url == "/"){
@@ -19,4 +21,40 @@ let server = http.createServer((request, response) => {
 
 server.listen(5050);
 
-console.log("Server Run Seccess");
+console.log("Server Run Seccess"); */
+
+
+
+/* 2. understanding package joson
+      open tarminal & comand: npm init --y
+      Adding a new command line to the script to make it easier to run node files:
+      "start": "node day2-main.js" 
+*/      
+
+
+//   3. understanding node URL module
+
+
+let http = require("http");
+let URL = require("url");
+
+let server = http.createServer((request, response) => {
+
+    let myURL = "http://rabbil.com/blog.html?year=2020&month=july";
+
+    let myURLObj = URL.parse(myURL, true);
+
+    let myHostName = myURLObj.host;
+    let mypathName = myURLObj.pathname;
+    let mySearchName = myURLObj.search;
+
+    response.writeHead(200, {"content-type": "text/html"})
+    response.write(myHostName)
+    response.end();
+
+
+});
+
+server.listen(5050);
+
+console.log("Server Run Seccess"); 
