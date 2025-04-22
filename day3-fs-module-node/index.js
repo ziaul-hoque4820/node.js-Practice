e429e08b29ea7d0f4449d2fb906b7cc554d47146
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-04-22 15:41:37 +0600

    fs-module readFileSync- Synchronous operation done

--- day3-fs-module-node/index.js
+++ day3-fs-module-node/index.js
@@ -7,16 +7,23 @@ let server = http.createServer((req, res) => {
     if(req.url == "/"){
 
         //Asynchronous operation
-        fs.readFile("home.html", (error, data) => {
+        /*fs.readFile("home.html", (error, data) => {
 
             res.writeHead(200, {"content-type": "text/html"});
             res.write(data);
             res.end();
-        })
+        }); */
+
+        
+        //Synchronous operation
+        let mydata = fs.readFileSync("home.html");
+        res.writeHead(200, {"content-type": "text/html"});
+        res.write(mydata);
+        res.end();
     }
     
 });
 
-server.listen(4040);
+server.listen(5050);
 
 console.log("Server Run Success");
