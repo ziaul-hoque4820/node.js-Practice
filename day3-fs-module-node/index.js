85f819b9a7205dca4ce2bc5768bab1e5f3313aab
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-04-22 20:04:58 +0600

    fs module- Asynchronous WriteFile operation

--- day3-fs-module-node/index.js
+++ day3-fs-module-node/index.js
@@ -4,24 +4,50 @@ let http = require("http");
 
 let server = http.createServer((req, res) => {
 
-    if(req.url == "/"){
+    if (req.url == "/") {
 
-        //Asynchronous operation
-        /*fs.readFile("home.html", (error, data) => {
+        //Asynchronous readFile operation
+        function readFileAsync() {
+            fs.readFile("home.html", (error, data) => {
+                res.writeHead(200, { "content-type": "text/html" });
+                res.write(data);
+                res.end();
+            });
+        };
 
-            res.writeHead(200, {"content-type": "text/html"});
-            res.write(data);
+
+
+        //Synchronous readFile operation
+        function readFileSync() {
+            let mydata = fs.readFileSync("home.html");
+            res.writeHead(200, { "content-type": "text/html" });
+            res.write(mydata);
             res.end();
-        }); */
+        };
 
+
+
+        //Asynchronous WriteFile operation
+        function writeFileAsync () {
+            fs.writeFile("demo.txt", "Wellcome to Node js practice", (error) => {
+
+                if (error) {
+                    res.writeHead(200, { "content-type": "text/html" });
+                    res.write('File Write Fail');
+                    res.end();
+                }
+                else {
+                    res.writeHead(200, { "content-type": "text/html" });
+                    res.write("File Write Success");
+                    res.end();
+                }
+            });
+        }
+
+        writeFileAsync();
         
-        //Synchronous operation
-        let mydata = fs.readFileSync("home.html");
-        res.writeHead(200, {"content-type": "text/html"});
-        res.write(mydata);
-        res.end();
     }
-    
+
 });
 
 server.listen(5050);
