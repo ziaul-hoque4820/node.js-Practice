04789aeb047f37ee1fc50c09572c32ee2b77d014
ziaul-hoque4820 <ziaulhoque4820@gmail.com>
2025-04-24 23:01:26 +0600

    Create Simple Website and run server side

---
+++ day5-fs-module-node-part-2/index.js
@@ -0,0 +1,30 @@
+let http = require("http");
+let fs = require("fs");
+
+let server = http.createServer((req, res) => {
+
+    if (req.url == "/") {
+        let data = fs.readFileSync("home.html", "utf8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    }
+    else if (req.url == "/contact") {
+        let data = fs.readFileSync("contact.html", "utf8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    }
+    else if (req.url == "/about") {
+        let data = fs.readFileSync("about.html", "utf8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    }
+    else if (req.url == "/terms") {
+        let data = fs.readFileSync("terms.html", "utf8");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+    }
+});
+
+server.listen(5050);
+
+console.log("Server Run Success");
